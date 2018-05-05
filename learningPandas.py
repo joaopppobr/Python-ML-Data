9e9f55c39d7aba2db69fba73d74264fe5e8164e0
joao pedro <joaopppobr@gmail.com>
2018-05-05 02:44:19 -0300

    Night One

---
+++ learningPandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pandas import DataFrame
+import datetime
+
+from pandas_datareader import data
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('airline_safety.csv')
+
+print (df.head())
+
+df[['incidents_00_14', 'fatal_accidents_00_14']].plot()
+plt.show()
